b11b12fde5e49e71814819e351408c5e624e0709
Warren Loy Harper II <warrenharper2@att.net>
2019-08-14 19:45:40 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,9 @@
-def using_while
-  levitation_force = 6
-	
-  def using_until
+def using_until
   levitation_force = 6
   while levitation_force < 10 
   puts "Wingardium Leviosa"
+  +=1
 end
 
-end
 
 
